83d351dffd51f078f1a3efa33ac7831ab1923cea
Leonardo <leonardopicolloto@gmail.com>
2021-08-23 00:27:33 -0400

    feature: add bootstrap css
    Signed-off-by: Leonardo <leonardopicolloto@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import 'bootstrap/scss/bootstrap.scss'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
